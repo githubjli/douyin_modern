0be8053d1715d4fd572335db2f41aad1faec7b0a
Jenny Li <root.jli@gmail.com>
2026-05-03 22:05:29 +0800

    chore: configure launcher icons with meow brand asset

---
+++ test/widget_test.dart
@@ -0,0 +1,14 @@
+import 'package:douyin_modern/app/app.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('app shell renders bottom tabs', (WidgetTester tester) async {
+    await tester.pumpWidget(const DouyinModernApp(enableFeedVideo: false));
+
+    expect(find.text('Home'), findsOneWidget);
+    expect(find.text('Discover'), findsOneWidget);
+    expect(find.text('Publish'), findsOneWidget);
+    expect(find.text('Messages'), findsOneWidget);
+    expect(find.text('Profile'), findsOneWidget);
+  });
+}

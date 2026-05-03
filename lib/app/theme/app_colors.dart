5c97b9fd37c4ef1176fa27695d923f0b9c6adf50
Jenny Li <root.jli@gmail.com>
2026-05-03 21:44:34 +0800

    chore: remove placeholder meow logo asset

---
+++ lib/app/theme/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+
+  static const Color warmBackground = Color(0xFFFFFCF3);
+  static const Color brandGold = Color(0xFFEFBC5C);
+  static const Color deepGold = Color(0xFFB8872E);
+  static const Color cocoaText = Color(0xFF745F40);
+  static const Color mutedOliveText = Color(0xFF948261);
+  static const Color inkDark = Color(0xFF2C2C2C);
+
+  static const Color cardBackground = Color(0xFFFFF9EC);
+  static const Color softBorder = Color(0xFFE8DBC1);
+}

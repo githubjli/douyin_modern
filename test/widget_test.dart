56388a695e2d28a60510c0d9cdd764be02a0d4be
Jenny Li <root.jli@gmail.com>
2026-05-04 21:21:13 +0700

    chore: set native app display name to Meow

---
+++ test/widget_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:meow_media/app/app.dart';
+
+void main() {
+  testWidgets('app shell renders updated bottom tabs', (WidgetTester tester) async {
+    await tester.pumpWidget(const DouyinModernApp(enableFeedVideo: false));
+
+    final Finder bottomNav = find.byType(BottomNavigationBar);
+    expect(bottomNav, findsOneWidget);
+
+    expect(find.descendant(of: bottomNav, matching: find.text('Home')), findsOneWidget);
+    expect(find.descendant(of: bottomNav, matching: find.text('Shorts')), findsOneWidget);
+    expect(find.descendant(of: bottomNav, matching: find.text('Membership')), findsOneWidget);
+    expect(find.descendant(of: bottomNav, matching: find.text('Profile')), findsOneWidget);
+  });
+}
